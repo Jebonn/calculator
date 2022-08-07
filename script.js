5bd33cf0d19378f000aa8d0f14c4006c7bc3cf0b
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-08-06 21:38:45 -0700

    Add basic arithmatic javascript functions

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operation, a, b) {
+    switch (operation) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "x":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
