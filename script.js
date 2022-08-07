31cd5be2011c41b19ccff39239a12144af0f976d
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-08-06 22:53:37 -0700

    Add highlight to active operation button

--- script.js
+++ script.js
@@ -35,12 +35,35 @@ function entry() {
     numbersDisplay.value += this.textContent;
 }
 
+function highlight(button) {
+    operationButtons.forEach(operation => {
+        if (button != operation) {
+            operation.style = "box-shadow: none;";
+        } else if (button == operation) {
+            button.style = "box-shadow: 0px 0px 1px 2px blue;";
+        }
+    })
+}
+
 const numbersDisplay = document.querySelector("#numbers-display");
-
 const clearButton = document.querySelector("#clear-entry");
-clearButton.addEventListener('click', clear);
-
+const equalButton = document.querySelector("#equal");
 const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operation");
+
 numberButtons.forEach(number => {
     number.addEventListener('click', entry);
 })
+
+operationButtons.forEach(operation => {
+    operation.addEventListener('click', () => {
+        highlight(operation); // highlights current operation button
+    })
+})
+
+equalButton.addEventListener('click', () => {
+    highlight(equalButton); // removes highlight from operation buttons
+})
+
+clearButton.addEventListener('click', clear);
+
