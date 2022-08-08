1a6ef18325329f38ab599887a98e0bf50cfb0520
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-08-07 19:31:41 -0700

    Bug fixes & add undo button

--- script.js
+++ script.js
@@ -18,23 +18,34 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operation, a, b) {
+function parseFix(num) {
+    return parseFloat(num);
+}
+
+function operate(operation) {
+    let [a, b] = equation.split(operation);
     a = +a;
     b = +b;
+
     switch (operation) {
         case "+":
-            display(add(a, b));
+            display(parseFix(add(a, b)));
             break;
         case "-":
-            display(subtract(a, b));
+            display(parseFix(subtract(a, b)));
             break;
         case "x":
-            display(multiply(a, b));
+            display(parseFix(multiply(a, b)));
             break;
         case "/":
-            display(divide(a, b));
+            if (b == 0) {
+                display("Don't do that."); // In case of divide by zero
+                break;
+            }
+            display(parseFix(divide(a, b)));
             break;
     }
+    equation = numbersDisplay.value;
 }
 
 function clear() {
@@ -42,87 +53,117 @@ function clear() {
 }
 
 function pi() {
-    display(Math.PI.toFixed(15));
+    display(Math.PI.toFixed(4));
 }
 
-// Adds pressed number to the display
-// Clears screen if gettingNewEntry is true. Caused by pressing
-// one of the +-x/ or = buttons, then sets it back to false
-function entry() {
-    if (gettingNewEntry) {
-        gettingNewEntry = false;
-        clear();
-    }
-    if (numbersDisplay.value.length <= 16) {
-        display(numbersDisplay.value + this.textContent);
-    }
+// Returns last character
+function lastValue() {
+    return equation.slice(-1);
+}
+
+// Returns true if equation variable has an operation symbol
+function hasOperation() {
+    return equation.match(/[-+/x]/);
 }
 
 // Adds a decimal point if there isn't one. Otherwise do nothing.
 function decimalEntry() {
     if (!numbersDisplay.value.includes(".")) {
-        display(numbersDisplay.value + this.textContent);
+        equation += ".";
+        display(numbersDisplay.value + ".");
     }
 }
 
-// Iterates through operationButtons NodeList
-// Highlights the arithmatic button that was pressed
-// Removes highlight from button not pressed or if CE or = buttons are pressed
-function highlight() {
+// Highlights if one of the +-x/ buttons were pressed
+// Removes highlight from buttons not pressed or if CE or = buttons were pressed
+function highlight(button) {
     operationButtons.forEach(operation => {
-        if (currentOperation != operation.textContent) {
+        if (button.textContent != operation.textContent || equation == '') {
             operation.style = "box-shadow: none;";
-        } else if (currentOperation == operation.textContent) {
+        } else if (button.textContent == operation.textContent) {
             operation.style = "box-shadow: 0px 0px 1px 2px blue;";
         }
     })
 }
 
-let currentOperation;           // Records current arithmatic operation
-let gettingNewEntry = false;    // True after pressing one of the +-x/ buttons
-let previousNum;                // Records previous numbers in display
+let activeOperation = '';   // Tracks current operation
+let equation = '';          // Records equation
 
-// QUERY SELECTORS
+////QUERY SELECTORS////
 const numbersDisplay = document.querySelector("#numbers-display");
 const clearButton = document.querySelector("#clear-entry");
 const equalButton = document.querySelector("#equal");
 const piButton = document.querySelector("#pi");
 const decimalButton = document.querySelector("#decimal");
+const inverseButton = document.querySelector("#inverse");
+const undoButton = document.querySelector("#undo");
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operation");
 
-// EVENT LISTENERS
+
+////EVENT LISTENERS////
 numberButtons.forEach(number => {
-    number.addEventListener('click', entry);
+    number.addEventListener('click', () => {
+        const buttonPress = number.textContent;
+
+        if (numbersDisplay.value == "Don't do that.") { // Divide by zero reset
+            clearButton.click();
+        }
+
+        if (lastValue().match(/[-+/x]/)) {
+            display(buttonPress)
+        } else if (lastValue() || equation == '') {
+            display(numbersDisplay.value + buttonPress);
+        } 
+        equation += buttonPress;
+    })
 })
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        // If an operation button was previously pressed, display the
-        // calculation before replacing it with the current operation
-        if (currentOperation != '') {
-            operate(currentOperation, previousNum, numbersDisplay.value);
+        const buttonPress = button.textContent;
+
+        if (lastValue().match(/[0-9]/) && hasOperation()) {
+            operate(activeOperation);
+        } else if (lastValue().match(/[-+/x]/)){
+            equation = equation.replace(/.$/, buttonPress);
+        } else {
+            equation += buttonPress;
         }
-        currentOperation = button.textContent;
-        gettingNewEntry = true; 
-        previousNum = numbersDisplay.value;
-        highlight();
+        activeOperation = buttonPress;
+        highlight(button);
     })
 })
 
 equalButton.addEventListener('click', () => {
-    operate(currentOperation, previousNum, numbersDisplay.value);
-    currentOperation = '';
-    gettingNewEntry = true;
-    highlight();
+    if (lastValue().match(/[0-9]/) && hasOperation()) {
+        operate(activeOperation);
+        highlight(equalButton);
+    }
 })
 
 clearButton.addEventListener('click', () => {
-    currentOperation = '';
-    highlight();
+    equation = '';
+    highlight(clearButton);
     clear();
 });
 
+inverseButton.addEventListener('click', () => {
+    if (Number(numbersDisplay.value) > 0) {
+        display("-" + numbersDisplay.value);
+    } else if (Number(numbersDisplay.value) < 0) {
+        display(numbersDisplay.value.slice(1));
+    }
+})
+
+undoButton.addEventListener('click', () => {
+    if (lastValue().match(/[0-9]/)){
+        display(numbersDisplay.value.slice(0, -1));
+    } else if (lastValue().match(/[-+/x]/)) {
+        highlight(clearButton)
+    }
+    equation = equation.slice(0, -1);
+})
+
 piButton.addEventListener('click', pi);
 decimalButton.addEventListener('click', decimalEntry);
-
